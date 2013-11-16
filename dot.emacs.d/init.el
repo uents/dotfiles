f4035e37c3620ad1e21c4fe4cd2dec0bf7027138
Takamasa Ueno <uents@ymail.com>
2013-11-17 01:29:09 +0900

    first commit

---
+++ dot.emacs.d/init.el
@@ -0,0 +1,372 @@
+;;;
+;;; @flie  ~/.emacs.d/init.el
+;;; @brief GNU Emacs configuration file (ver 24.3 or later)
+;;;
+
+;;----------------------------------------------------------
+;; General
+;;----------------------------------------------------------
+
+;; Japanese Environment
+;; 他のあらゆる設定よりも必ず前で行う
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+
+;; network proxy
+;(setq url-proxy-services
+;	  '(("http" . "example.com:8080")
+;		("no_proxy" . "127.0.0.1")))
+ 
+;; ELPA
+;; パッケージの初期パスは ~/.emacs.d/elisp
+(when (require 'package nil t)
+  (add-to-list 'package-archives
+			   '("melpa" . "http://melpa.milkbox.net/packages/"))
+  (add-to-list 'package-archives
+			   '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (package-initialize)
+)
+
+;; auto-install
+;; M-x package-list-packages RET auto-install
+(when (require 'auto-install nil t)
+  ;; インストールディレクトリを設定
+  ;; 初期値は ~/.emacs.d/auto-install/
+;  (setq auto-install-directory "~/.emacs.d/auto-install")
+
+  ;; インストールディレクトリをload-pathに追加
+  (add-to-list 'load-path "~/.emacs.d/auto-install")
+
+  ;; EmacsWikiに登録されているパッケージリストを自動的に取得
+;  (auto-install-update-emacswiki-package-name t)
+)
+
+;; inhibit loading "default.el" at startup
+(setq inhibit-default-init t)
+
+;; 起動メッセージを非表示
+(setq inhibit-startup-screen t)
+
+;; バックアップファイルの作成を禁止
+(setq backup-inhibited t)
+
+;; 終了時に自動保存ファイルを削除
+(setq delete-auto-save-files t)
+
+;; バッファ末尾に余計な改行コードを防ぐ
+(setq next-line-add-newlines nil)
+
+;; デフォルトのタブ幅
+(setq-default tab-width 4)
+
+;; key bindings
+(global-set-key "\C-h" 'backward-delete-char)
+(global-set-key "\C-\\" 'ignore)
+(global-set-key [?¥] [?\\])  ;; ¥の代わりに\を入力
+
+
+;;----------------------------------------------------------
+;; Look & Feel
+;;----------------------------------------------------------
+
+;; 行番号・列番号を表示
+(line-number-mode t)
+(column-number-mode t)
+
+;; 時間を表示
+(display-time)
+
+;; スクロールバーを右に配置
+;(set-scroll-bar-mode 'right)
+
+;; 対応する括弧を強調表示
+(setq show-paren-delay 0) ;; 0秒(遅延なし)で表示
+(show-paren-mode t)
+
+;; 選択リージョン範囲を可視化
+(setq-default transient-mark-mode t)
+
+;; 長い行は折り返す
+(setq truncate-lines t)
+
+;; 行間を指定
+(setq-default line-spacing 2)
+
+(when window-system
+  ;; メニューバーを非表示
+  (menu-bar-mode -1)
+  ;; タイトル
+  (setq frame-title-format (concat "%b"))
+  (setq default-frame-alist
+	    '(
+	      ;; ウィジェットのサイズ
+;	      (width . 80)
+;	      (height . 52)
+	      ))
+  ;; フォント
+  ;; (prin1 (font-family-list)) でfont-familyの出力可能
+  (set-face-attribute 'default nil
+					  :height 120
+;					  :family "Monaco"
+					  :family "Meslo LG M DZ"
+					  )
+  (set-fontset-font nil 'japanese-jisx0208
+					(font-spec
+					 :family "Hiragino Kaku Gothic ProN"))
+  (setq face-font-rescale-alist
+		'(("^-apple-hiragino.*" . 1.0)
+		  (".*osaka-bold.*" . 1.0)
+		  (".*osaka-medium.*" . 1.0)
+		  (".*courier-bold-.*-mac-roman" . 1.0)
+		  (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+		  (".*monaco-bold-.*-mac-roman" . 0.9)
+		  ("-cdac$" . 1.3)))
+)
+
+;; color-theme
+;; M-x package-list-packages RET color-theme
+(when (locate-library "color-theme")
+  (color-theme-initialize)
+  (color-theme-midnight)
+)
+
+;;----------------------------------------------------------
+;; input method
+;;----------------------------------------------------------
+
+;; mozc
+;(require 'mozc)
+;(load-file "/usr/local/share/emacs/23.4/site-lisp/mozc.el")
+;(setq default-input-method "japanese-mozc")
+;(global-set-key (kbd "C-\\") 'toggle-input-method)
+
+;; anthy
+;(add-to-list 'load-path "/usr/share/emacs/site-lisp/anthy")
+;(require 'anthy)
+;(setq default-input-method "japanese-anthy")
+; Cant't activate input method 'japanese-anthy' と表示される場合必要
+;(load-library "leim-list")
+;(setq default-input-method "japanese-anthy")
+
+;;----------------------------------------------------------
+;; GDB/GUD
+;;----------------------------------------------------------
+(defvar gud-gdb-history (list "gdb --annotate=3 "))
+(setq gdb-many-windows t)
+(setq gdb-use-sparate-io-buffer t)
+
+(add-hook 'gdb-mode-hook
+	  '(lambda ()
+	     ;; key bindings
+;	     (local-set-key  [f2] 'gud-remove)
+;	     (local-set-key  [f3] 'gud-display)
+;	     (local-set-key  [f4] 'gud-print)
+;	     (local-set-key  [f5] 'gud-cont)
+;	     (local-set-key  [f6] 'gud-finish)
+;	     (local-set-key  [f7] 'gud-run)
+;	     (local-set-key  [f9] 'gud-break)
+;	     (local-set-key  [f10] 'gud-next)
+;	     (local-set-key  [f11] 'gud-step)
+;	     (setq mode-line-format
+;		   " F2:remove F3:disp F4:print F5:cont F6:fin F7:run F9:break F10:next F11:step")
+	     ;; カーソルが置かれた変数の値を表示
+	     (gud-tooltip-mode t)
+	     ;;
+	     (gud-break-main nil)
+	     (gud-run nil)
+	     ))
+
+;;----------------------------------------------------------
+;; helm
+;;  helm
+;;   M-x package-list-packages RET helm
+;;  helm-ack
+;;   M-x package-list-packages RET helm-ack
+;;  helm-c-moccur
+;;   M-x package-list-packages RET color-moccur
+;;   M-x package-list-packages RET helm-c-moccur
+;;  helm-gtags
+;;   M-x package-list-packages RET helm-gtags
+;;  helm-etags-plus
+;;   git clone https://github.com/jixiuf/helm-etags-plus.git
+;;----------------------------------------------------------
+
+;; helm
+(when (require 'helm-config nil t)
+  (helm-mode 1)
+  (global-set-key (kbd "C-x b") 'helm-mini)
+  (global-set-key (kbd "C-M-z") 'helm-resume)
+  ;; helm-find-filesの自動補完を無効にしTABで手動補完する
+  (custom-set-variables '(helm-ff-auto-update-initial-value nil))
+  (define-key helm-c-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-c-read-file-map (kbd "C-h") 'delete-backward-char)
+ )
+
+;; helm-ack
+(when (require 'helm-ack nil t)
+  (setq helm-c-ack-version 2)
+  ;; does not insert '--type' option
+  (setq helm-c-ack-auto-set-filetype nil)
+  ;; insert "thing-at-point 'symbol" as search pattern
+  (setq helm-c-ack-thing-at-point 'symbol)
+  ;; key bindings
+  (global-set-key (kbd "C-x g") 'helm-ack)
+)
+
+;; helm-c-moccur
+(when (require 'helm-c-moccur nil t)
+  (setq helm-c-moccur-helm-idle-delay 0.1)
+  ;; `helm-c-moccur-dmoccur などのコマンドでバッファの情報をハイライトする
+  (setq helm-c-moccur-higligt-info-line-flag t)
+  ;; 現在選択中の候補の位置を他のwindowに表示する
+  (setq helm-c-moccur-enable-auto-look-flag t)
+  ;; `helm-c-moccur-occur-by-moccur の起動時にポイントの位置の単語を初期パターンにする
+  (setq helm-c-moccur-enable-initial-pattern t)
+  ;; key bindings
+  (define-key global-map (kbd "M-o") 'helm-c-moccur-occur-by-moccur) ;バッファ内検索
+  (define-key global-map (kbd "M-d") 'helm-c-moccur-dmoccur) ;ディレクトリ
+  (define-key global-map (kbd "s-o") 'helm-c-moccur-buffer-list)
+  (define-key isearch-mode-map (kbd "M-o") 'helm-c-moccur-from-isearch)
+;
+;  (defun dired-mode-hook-for-helm-c-moccur ()
+;	(local-set-key (kbd "O") 'helm-c-moccur-dired-do-moccur-by-moccur))
+;  (add-hook 'dired-mode-hook 'dired-mode-hook-for-helm-c-moccur)
+)
+
+;; helm-gtags
+(when (require 'helm-gtags nil t)
+  (add-hook 'helm-gtags-mode-hook
+			'(lambda ()
+			   (setq helm-gtags-ignore-case t)
+			   ;; key bindings
+			   (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
+			   (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
+			   (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+			   (local-set-key (kbd "C-q") 'helm-gtags-pop-stack)
+			   (local-set-key (kbd "C-c C-f") 'helm-gtags-find-files)))
+)
+
+;; helm-etags-plus
+(add-to-list 'load-path "~/.emacs.d/local/helm-etags-plus")
+(when (require 'helm-etags+ nil t)
+  (global-set-key (kbd "M-.") 'helm-etags+-select)
+  (global-set-key (kbd "M-*") 'helm-etags+-history-go-back)
+)
+
+;;----------------------------------------------------------
+;; yasnippet
+;; M-x package-list-packages RET yasnippet
+;;----------------------------------------------------------
+
+(when (require 'yasnippet nil t)
+  (setq yas-snippet-dirs
+		'("~/.emacs.d/local/yasnippet/snippets"
+;		  "~/.emacs.d/local/yasnippet/extras/imported"
+		  ))
+  (yas-global-mode 1)
+)
+
+;;----------------------------------------------------------
+;; Auto Complete Mode
+;;  auto-complete
+;;   M-x package-list-packages RET auto-complete
+;;  ac-dict/js2-mode
+;;   wget https://raw.github.com/sandai/dotfiles/master/.emacs.d/ac-dict/js2-mode
+;;----------------------------------------------------------
+
+;; Auto Complete Mode
+(when (require 'auto-complete-config nil t)
+  (add-to-list 'ac-modes 'js2-mode)
+  (add-to-list 'ac-modes 'web-mode)
+  (ac-config-default) ;elisp,c,ruby,cssの各モードにhook
+  (setq ac-auto-start 4)
+  (setq ac-dwim t)
+  (setq ac-use-menu-map t) ;C-n/C-pで候補選択可能
+  (setq ac-comphist-file "~/.emacs.d/local/ac-comphist.dat") ;補完履歴のキャッシュ先
+)
+
+;;----------------------------------------------------------
+;; C
+;;----------------------------------------------------------
+
+(add-hook 'c-mode-common-hook
+	  '(lambda ()
+		 ;; gtagsを有効に
+		 (helm-gtags-mode t)
+		 ;; flymakeを有効に
+;		 (flymake-mode t)
+
+		 ;; [TAB]キーの振舞い
+		 ;;   t => [TAB]キーでインデントを空ける
+		 ;; nil => [TAB]キーで[TAB]コードを入力する
+		 (setq c-tab-always-indent . nil)
+		 ;; タブ幅の設定
+		 (setq tab-width 8)
+		 ;; インデント量の設定
+		 (setq c-basic-offset 8)
+		 (setq c-comment-only-line-offset . 0)
+	     ))
+
+;;----------------------------------------------------------
+;; JavaScript
+;;  js2-mode
+;;   M-x package-list-packages RET js2-mode
+;;----------------------------------------------------------
+
+;; js2-mode
+(when (require 'js2-mode nil t)
+  (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+)
+
+;;----------------------------------------------------------
+;; HTML/CSS
+;;  web-mode
+;;   M-x package-list-packages RET web-mode
+;;----------------------------------------------------------
+
+(when (require 'web-mode nil t)
+  (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+  (add-hook 'web-mode-hook
+		(lambda()
+		  ;; indent offset
+		  (setq web-mode-markup-indent-offset 2)
+		  (setq web-mode-css-indent-offset 2)
+		  (setq web-mode-code-indent-offset 2)
+		  ;; face color
+		   (custom-set-faces
+			'(web-mode-doctype-face
+			  ((t (:foreground "#00CCCC"))))
+			'(web-mode-html-tag-face
+			  ((t (:foreground "#6666FF"))))
+			'(web-mode-html-attr-name-face
+			  ((t (:foreground "#CC3333"))))
+			'(web-mode-html-attr-value-face
+			  ((t (:foreground "#FF66FF"))))
+			'(web-mode-comment-face
+			  ((t (:foreground "#00CC00"))))
+			'(web-mode-server-comment-face
+			  ((t (:foreground "#00CC00"))))
+			'(web-mode-css-rule-face
+			  ((t (:foreground "#A0D8EF"))))
+			'(web-mode-css-pseudo-class-face
+			  ((t (:foreground "#FF7F00"))))
+			'(web-mode-css-at-rule-face
+			  ((t (:foreground "#FF7F00"))))
+			)
+		   ))
+)
+
+;;----------------------------------------------------------
+;; Common Lisp
+;;  slime
+;;   M-x package-list-packages RET slime
+;;----------------------------------------------------------
+
+(when (require 'slime nil t)
+  (setq inferior-lisp-program "/usr/local/bin/clisp")
+)
+
---
+++ dot.zshrc
@@ -0,0 +1,120 @@
+# -*- mode: shell-script -*-
+# @file ~/.zshrc
+
+export LANG=ja_JP.UTF-8
+
+## ヒストリファイルの設定
+HISTFILE=$HOME/.zsh-history
+HISTSIZE=100000
+SAVEHIST=100000
+
+## 開始/終了時刻をヒストリファイルに書き込む
+setopt extended_history
+
+## ヒストリを共有
+setopt share_history
+
+## 全履歴の一覧を出力する
+function history-all { history -E 1 }
+
+## 補完機能の強化
+autoload -U compinit
+compinit
+
+## コアダンプサイズを制限
+limit coredumpsize 102400
+
+## 出力の文字列末尾に改行コードが無い場合でも表示
+unsetopt promptcr
+
+## 色を使う
+setopt prompt_subst
+
+## ビープを鳴らさない
+setopt nobeep
+
+## 内部コマンド jobs の出力をデフォルトで jobs -l にする
+setopt long_list_jobs
+
+## 補完候補一覧でファイルの種別をマーク表示
+setopt list_types
+
+## サスペンド中のプロセスと同じコマンド名を実行した場合はリジューム
+setopt auto_resume
+
+## 補完候補を一覧表示
+setopt auto_list
+
+## 直前と同じコマンドをヒストリに追加しない
+setopt hist_ignore_dups
+
+## cd 時に自動で push
+setopt autopushd
+
+## 同じディレクトリを pushd しない
+setopt pushd_ignore_dups
+
+## ファイル名で #, ~, ^ の 3 文字を正規表現として扱う
+setopt extended_glob
+
+## TAB で順に補完候補を切り替える
+setopt auto_menu
+
+## =command を command のパス名に展開する
+setopt equals
+
+## --prefix=/usr などの = 以降も補完
+setopt magic_equal_subst
+
+## ヒストリを呼び出してから実行する間に一旦編集
+setopt hist_verify
+
+# ファイル名の展開で辞書順ではなく数値的にソート
+setopt numeric_glob_sort
+
+## 出力時8ビットを通す
+setopt print_eight_bit
+
+## 補完候補のカーソル選択を有効に
+zstyle ':completion:*:default' menu select=1
+
+## 補完候補の色づけ
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
+## ディレクトリ名だけで cd
+setopt auto_cd
+
+## カッコの対応などを自動的に補完
+setopt auto_param_keys
+
+## ディレクトリ名の補完で末尾の / を自動的に付加し、次の補完に備える
+setopt auto_param_slash
+
+## スペルチェック
+setopt correct
+
+## Emacsと同じキー操作を行う
+bindkey -e
+
+
+## ibus-mozc
+#mozc_server_start
+#export XIM=ibus
+#export GTK_IM_MODULE=ibus
+#export QT_IM_MODULE=xim
+#export XMODIFIERS=@im=ibus
+#export XIM_PROGRAM=ibus-daemon
+#export XIM_ARGS="-r --daemonize --xim"
+
+## aliases
+#alias ls="ls -F --color"
+alias ls="ls -F -G"
+alias emacs="TERM=xterm-256color XMODIFIERS=@im=none /usr/local/bin/emacs"
+alias mc="minicom configuration dfl"
+alias mc2="minicom configuration dfl2"
+alias plantuml="java -jar ${HOME}/opt/plantuml/plantuml.jar"
+alias irb="irb --readline -r irb/completion"
+
+export NODE_PATH=/usr/local/lib/node
+export PATH=$HOME/opt/bin:$PATH
+
